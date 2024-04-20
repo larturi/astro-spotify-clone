466a4d94809b1766c30b7248d58a4339617f9480
Leandro Arturi <lea.arturi@gmail.com>
2024-04-20 19:05:05 -0300

    Componente Greatting con Svelte

---
+++ src/components/Greeting.svelte
@@ -0,0 +1,16 @@
+<script lang="ts">
+    const currentTime = new Date()
+    const currentHour = currentTime.getHours()
+
+    let gretting = ""
+
+    if(currentHour < 12) {
+        gretting = "Buenos días"
+    } else if(currentHour < 18) {
+        gretting = "Buenas tardes"
+    } else {
+        gretting = "Buenas noches"
+    }
+</script>
+
+<h1 class="text-3xl font-bold text-white">{gretting}</h1>
