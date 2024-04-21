d3d191742304bd30070b859a6bda5a2ba8f5fdce
Leandro Arturi <lea.arturi@gmail.com>
2024-04-21 12:03:41 -0300

    Vercel adapter to deploy

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,6 @@
 import { defineConfig } from 'astro/config'
 
-import vercel from '@astrojs/vercel'
+import vercel from '@astrojs/vercel/serverless'
 import tailwind from '@astrojs/tailwind'
 import svelte from '@astrojs/svelte'
 
