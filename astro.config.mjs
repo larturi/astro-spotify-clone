c7a1e8524bdb6639bff2cd05f09398242dc97cec
Leandro Arturi <lea.arturi@gmail.com>
2024-04-21 12:00:53 -0300

    Vercel adapter to deploy

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,14 @@
-import { defineConfig } from 'astro/config';
-import tailwind from '@astrojs/tailwind';
-import svelte from "@astrojs/svelte";
+import { defineConfig } from 'astro/config'
 
-import react from "@astrojs/react";
+import vercel from '@astrojs/vercel'
+import tailwind from '@astrojs/tailwind'
+import svelte from '@astrojs/svelte'
+
+import react from '@astrojs/react'
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind(), svelte(), react()],
-  output: 'server'
-});
+  output: 'server',
+  adapter: vercel()
+})
